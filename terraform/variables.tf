9e2779746556eea8287fa1f85602d6fd54b0b7b8
glutenfree <glutenfree@Mac-mini-de-glutenfree.local>
2025-09-05 19:43:01 +0200

    :wheelchair: boom

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,22 +2,24 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
-  default     = "rg-laravel-app"
+  default     = "rg-par_10"
 }
 
 variable "location" {
   description = "Azure region"
   type        = string
-  default     = "West Europe"
+  default     = "France Central"
 }
 
 variable "app_name" {
   description = "App Service name (must be globally unique)"
   type        = string
+  default     = "Laravel"
 }
 
 variable "app_key" {
   description = "Laravel APP_KEY"
   type        = string
   sensitive   = true
+  default     = "base64:DJYTvaRkEZ/YcQsX3TMpB0iCjgme2rhlIOus9A1hnj4="
 }
